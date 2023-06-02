2b818e1a93aefefae2c5f7d460648abb568cd2fe
Jadranko90 <jadrankojankovic@live.com>
2023-06-02 09:23:15 +0200

    initital project setup

---
+++ src/main/java/helpers/FileControlUtil.java
@@ -0,0 +1,18 @@
+package helpers;
+
+import java.util.Properties;
+
+public class FileControlUtil {
+    public Properties properties;
+
+    public static final String FE_URL = "fe_routing.properties";
+
+    public FileControlUtil(String file) throws Exception {
+        properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/" + file));
+    }
+
+    public String getValue(String propertyKey) {
+        return  properties.getProperty(propertyKey);
+    }
+}
